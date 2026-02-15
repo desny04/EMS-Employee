cdec76210b4b96d62c498160748bbf0c3ab793af
desny <trsadesny@gmail.com>
2026-02-15 12:09:12 +0530

    models created

--- ems/app/models.py
+++ ems/app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Employee(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    phone = models.CharField(max_length=15)
+    department = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
+class Work(models.Model):
+
+    STATUS_CHOICES = [
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('On Testing', 'On Testing'),
+        ('Done', 'Done'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    assigned_date = models.DateField()
+    deadline = models.DateField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='To Do'
+    )
+
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
