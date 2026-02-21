734f7a1ee0e003fb9233b1f376c11f4ed3be6e15
desny <trsadesny@gmail.com>
2026-02-21 22:21:19 +0530

    admin dashboard

--- ems/app/urls.py
+++ ems/app/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.login_view,),
     path('logout/',views.logout_view,name='logout'),
+
     path('admin-dashboard/',views.admin_dashboard,name='admin_dashboard'),
+    # path('admin/add-employee/',views.add_employee,name='add_employee'),
+    # path('admin/employees/',views.employee_list,name='employee_list'),
+    # path('admin/edit/<int:id>/',views.edit_employee, name='edit_employee'),
+    # path('admin/delete/<int:id>/',views.delete_employee,name='delete_employee'),
+    # path('admin/assign-work/',views.assign_work, name='assign_work'),
 ]
